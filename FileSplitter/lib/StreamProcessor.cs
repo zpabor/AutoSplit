253c885d51944f4bd5c04b7b53fc56e16b0fdf79
zpabor <zpabor@gmail.com>
2016-02-24 17:40:49 -0600

    added more code

--- FileSplitter/lib/StreamProcessor.cs
+++ FileSplitter/lib/StreamProcessor.cs
@@ -9,36 +9,51 @@ namespace FileSplitter
 {
     abstract class StreamProcessor
     {
-        RingBuffer rbuff;
-        private Task t;
-        private Stream outputStream;
-        public void CreateTask()
+        public RingBuffer ringbuffer { set { _rbuff = value; } }
+        private RingBuffer _rbuff;      
+        public Stream outputStream { set { _outputStream = value; } }
+        private Stream _outputStream;
+        private Task _task;
+        public Task proctask { get { if (_task == null) CreateTask(); return _task; } }
+        public StreamProcessor(Stream outstream, ref RingBuffer rbuffinit)
+        {
+            outputStream = outstream;
+            _rbuff = rbuffinit;
+        }  
+        private void CreateTask()
         {
             Task processorTask = new Task(() =>
             {
                 byte[] tmpbuff = new byte[1];
                 
-                for (long ia = 0; ia < (outputStream.Length / tmpbuff.Length) - 1; ia++)
+                for (long ia = 0; ia < (_outputStream.Length / tmpbuff.Length) - 1; ia++)
                 {
-                    tmpbuff = rbuff.readNext();
+                    tmpbuff = _rbuff.readNext();
                     buffworker(ref tmpbuff);                   
                 }
-                tmpbuff = rbuff.readNext();
+                tmpbuff = _rbuff.readNext();
                 buffworker(ref tmpbuff);                
             });
-
+            _task = processorTask;
         }
         public abstract void buffworker(ref byte[] objectivebuffer);
     }
+
+
     class StreamProcessorController
     {
-        Task[] taskset;
-
+        List<StreamProcessor> _streamProcList;
+        private RingBuffer _rbuff;
         public void Start()
         {
-            RingBuffer rbuff = new RingBuffer(1024, taskset.Length);
+            _rbuff = new RingBuffer(1024, taskset.Length);
             foreach (Task t in taskset)
                 t.Start();      
+        }
+        public void addProcessor(StreamProcessor newStreamProc)
+        {
+            newStreamProc.ringbuffer = _rbuff;
+            _streamProcList.Add(newStreamProc);
         }
     }
 }
